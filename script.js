41ed21ae3e2d5baccd7a1d887c6dbae0e5ddc040
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-04-08 17:44:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const afterImg = document.querySelector('.after-img');
   const handle = document.querySelector('.handle');
   const handleContainer = document.querySelector('.handle-container');
+  const sliderBar = document.createElement('div');
+  sliderBar.className = 'slider-bar';
+  sliderContainer.appendChild(sliderBar);
 
   function updateSlider(e) {
     const rect = sliderContainer.getBoundingClientRect();
@@ -10,18 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const width = sliderContainer.clientWidth;
     const position = offsetX / width;
     afterImg.style.clipPath = `inset(0 ${100 - position * 100}% 0 0)`;
-    handle.style.left = `${position * 100}%`;
+    sliderBar.style.left = `${position * 100}%`;
   }
 
-  handleContainer.addEventListener('mousedown', function () {
-    handleContainer.addEventListener('mousemove', updateSlider);
-  });
-
-  handleContainer.addEventListener('mouseup', function () {
-    handleContainer.removeEventListener('mousemove', updateSlider);
-  });
+  handleContainer.addEventListener('mousemove', updateSlider);
 
   handleContainer.addEventListener('mouseleave', function () {
-    handleContainer.removeEventListener('mousemove', updateSlider);
+    afterImg.style.clipPath = 'inset(0 70% 0 0)';
+    sliderBar.style.left = '30%';
+  });
+
+  handleContainer.addEventListener('mouseenter', function () {
+    afterImg.style.clipPath = 'inset(0 50% 0 0)';
+    sliderBar.style.left = '50%';
   });
 });
