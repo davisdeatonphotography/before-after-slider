a086da63da751df15ee93c1c2c38b595859b1ab4
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-04-08 16:32:27 -0500

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,14 +1,19 @@
 const sliderContainer = document.querySelector('.slider-container');
 const beforeImage = document.querySelector('.before-image');
+const sliderBar = document.createElement('div');
+sliderBar.className = 'slider-bar';
+sliderContainer.appendChild(sliderBar);
 
 sliderContainer.addEventListener('mousemove', (event) => {
   const offsetX = event.clientX - sliderContainer.getBoundingClientRect().left;
   const width = sliderContainer.clientWidth;
   const percentage = (offsetX / width) * 100;
 
-  beforeImage.style.width = `${percentage}%`;
+  beforeImage.style.width = `${100 - percentage}%`;
+  sliderBar.style.left = `${offsetX}px`;
 });
 
 sliderContainer.addEventListener('mouseleave', () => {
   beforeImage.style.width = '50%';
+  sliderBar.style.left = '50%';
 });
