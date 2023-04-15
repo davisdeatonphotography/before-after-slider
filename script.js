e009a3f6d3bc623bb072c026c771524953a33ec1
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-04-15 10:34:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+
+### JavaScript Functionality
+```javascript
 document.addEventListener('DOMContentLoaded', function () {
   const imageComparison = document.querySelector('.image-comparison');
   const afterImage = document.querySelector('.after-image');
   const slider = document.querySelector('.slider');
 
-  imageComparison.addEventListener('mousemove', function (event) {
+  function updateSliderPosition(event) {
     const rect = imageComparison.getBoundingClientRect();
-    const mouseXPercentage = (event.clientX - rect.left) / rect.width;
+    const mouseXPercentage = ((event.clientX || event.touches[0].clientX) - rect.left) / rect.width;
     const clipWidth = rect.width * mouseXPercentage;
     afterImage.style.clipPath = `inset(0 ${rect.width - clipWidth}px 0 0)`;
     slider.style.left = `${clipWidth}px`;
-  });
+  }
+
+  imageComparison.addEventListener('mousemove', updateSliderPosition);
+  imageComparison.addEventListener('touchmove', updateSliderPosition);
 });
