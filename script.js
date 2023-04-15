41fc8dd7df592c1025d4d0051fe20ab5aaf99e61
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-04-15 10:04:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,13 @@
-const slider = document.getElementById('before-after-slider');
-const before = document.getElementById('before-image');
-const beforeImage = before.getElementsByTagName('img')[0];
-const resizer = document.getElementById('resizer');
+document.addEventListener('DOMContentLoaded', function () {
+  const imageComparison = document.querySelector('.image-comparison');
+  const afterImage = document.querySelector('.after-image');
+  const slider = document.querySelector('.slider');
 
-let active = false;
-
-//Sort overflow out for Overlay Image
-document.addEventListener("DOMContentLoaded", function() {
-  let width = slider.offsetWidth;
-  console.log(width);
-  beforeImage.style.width = width + 'px';
+  imageComparison.addEventListener('mousemove', function (event) {
+    const rect = imageComparison.getBoundingClientRect();
+    const mouseXPercentage = (event.clientX - rect.left) / rect.width;
+    const clipWidth = rect.width * mouseXPercentage;
+    afterImage.style.clipPath = `inset(0 ${rect.width - clipWidth}px 0 0)`;
+    slider.style.left = `${clipWidth}px`;
+  });
 });
-
-//Adjust width of image on resize 
-window.addEventListener('resize', function() {
-  let width = slider.offsetWidth;
-  console.log(width);
-  beforeImage.style.width = width + 'px';
-})
-
-resizer.addEventListener('mousedown',function(){
-  active = true;
- resizer.classList.add('resize');
-
-});
-
-document.body.addEventListener('mouseup',function(){
-  active = false;
- resizer.classList.remove('resize');
-});
-
-document.body.addEventListener('mouseleave', function() {
-  active = false;
-  resizer.classList.remove('resize');
-});
-
-document.body.addEventListener('mousemove',function(e){
-  if (!active) return;
-  let x = e.pageX;
-  x -= slider.getBoundingClientRect().left;
-  slideIt(x);
-  pauseEvent(e);
-});
-
-resizer.addEventListener('touchstart',function(){
-  active = true;
-  resizer.classList.add('resize');
-});
-
-document.body.addEventListener('touchend',function(){
-  active = false;
-  resizer.classList.remove('resize');
-});
-
-document.body.addEventListener('touchcancel',function(){
-  active = false;
-  resizer.classList.remove('resize');
-});
-
-//calculation for dragging on touch devices
-document.body.addEventListener('touchmove',function(e){
-  if (!active) return;
-  let x;
-  
-  let i;
-  for (i=0; i < e.changedTouches.length; i++) {
-  x = e.changedTouches[i].pageX; 
-  }
-  
-  x -= slider.getBoundingClientRect().left;
-  slideIt(x);
-  pauseEvent(e);
-});
-
-function slideIt(x){
-    let transform = Math.max(0,(Math.min(x,slider.offsetWidth)));
-    before.style.width = transform+"px";
-    resizer.style.left = transform-0+"px";
-}
-
-//stop divs being selected.
-function pauseEvent(e){
-    if(e.stopPropagation) e.stopPropagation();
-    if(e.preventDefault) e.preventDefault();
-    e.cancelBubble=true;
-    e.returnValue=false;
-    return false;
-}
-
