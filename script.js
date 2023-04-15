751e2790a1deaec0a4b11bf0f2346bdf90b0fde1
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-04-15 15:46:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
 const slider = document.querySelector('.slider');
-const afterImage = document.querySelector('.after-image');
+const handle = document.querySelector('.handle');
+const beforeImage = document.querySelector('.before-image');
 const imageContainer = document.querySelector('.image-container');
 
 let dragging = false;
 let containerWidth = imageContainer.clientWidth;
 
 function updateSlider(x) {
-	const percentage = (x / containerWidth) * 100;
-	afterImage.style.clipPath = `inset(0 ${100 - percentage}% 0 0)`;
-	slider.style.left = `${x}px`;
+    const percentage = (x / containerWidth) * 100;
+    beforeImage.style.width = `${percentage}%`;
+    handle.style.left = `${x}px`;
 }
 
 function onMouseDown(event) {
-	event.preventDefault();
-	dragging = true;
+    event.preventDefault();
+    dragging = true;
 }
 
 function onMouseMove(event) {
-  if (!dragging) return;
-  const x = Math.max(0, Math.min(event.pageX - imageContainer.getBoundingClientRect().left, containerWidth - slider.clientWidth));
-  updateSlider(x);
+    if (!dragging) return;
+    const x = Math.max(0, Math.min(event.pageX - imageContainer.getBoundingClientRect().left, containerWidth));
+    updateSlider(x);
 }
 
 function onMouseUp() {
-	dragging = false;
+    dragging = false;
 }
 
 function onTouchStart(event) {
-	event.preventDefault();
-	dragging = true;
+    event.preventDefault();
+    dragging = true;
 }
 
 function onTouchMove(event) {
-  if (!dragging) return;
-  const x = Math.max(0, Math.min(event.touches[0].pageX - imageContainer.getBoundingClientRect().left, containerWidth - slider.clientWidth));
-  updateSlider(x);
+    if (!dragging) return;
+    const x = Math.max(0, Math.min(event.touches[0].pageX - imageContainer.getBoundingClientRect().left, containerWidth));
+    updateSlider(x);
 }
 
 function onTouchEnd() {
-	dragging = false;
+    dragging = false;
 }
 
-slider.addEventListener('mousedown', onMouseDown);
+handle.addEventListener('mousedown', onMouseDown);
 document.addEventListener('mousemove', onMouseMove);
 document.addEventListener('mouseup', onMouseUp);
 
-slider.addEventListener('touchstart', onTouchStart);
+handle.addEventListener('touchstart', onTouchStart);
 document.addEventListener('touchmove', onTouchMove);
 document.addEventListener('touchend', onTouchEnd);
 
 window.addEventListener('resize', () => {
-	containerWidth = imageContainer.clientWidth;
+    containerWidth = imageContainer.clientWidth;
 });
