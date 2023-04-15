2957a035ab76e3e8f2b95ef5ffe36e9a3945c902
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-04-15 10:51:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const imagesContainer = document.querySelector('.images-container');
+	const afterImage = document.querySelector('.after-image');
+	const slider = document.querySelector('.slider');
 
-### JavaScript Functionality
-```javascript
-document.addEventListener('DOMContentLoaded', function () {
-  const imageComparison = document.querySelector('.image-comparison');
-  const afterImage = document.querySelector('.after-image');
-  const slider = document.querySelector('.slider');
+	function updateSliderPosition(event) {
+		const rect = imagesContainer.getBoundingClientRect();
+		const mouseXPercentage = ((event.clientX || event.touches[0].clientX) - rect.left) / rect.width;
+		const clipWidth = rect.width * mouseXPercentage;
+		afterImage.style.clipPath = `inset(0 ${rect.width - clipWidth}px 0 0); slider.style.left = ${clipWidth}px`;
+	}
 
-  function updateSliderPosition(event) {
-    const rect = imageComparison.getBoundingClientRect();
-    const mouseXPercentage = ((event.clientX || event.touches[0].clientX) - rect.left) / rect.width;
-    const clipWidth = rect.width * mouseXPercentage;
-    afterImage.style.clipPath = `inset(0 ${rect.width - clipWidth}px 0 0)`;
-    slider.style.left = `${clipWidth}px`;
-  }
-
-  imageComparison.addEventListener('mousemove', updateSliderPosition);
-  imageComparison.addEventListener('touchmove', updateSliderPosition);
+	imagesContainer.addEventListener('mousemove', updateSliderPosition);
+	imagesContainer.addEventListener('touchmove', updateSliderPosition);
 });
